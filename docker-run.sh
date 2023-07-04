aef197a885b955b88ca5a1d515c71cce1c9611a1
iphearum <phearumgic@gmail.com>
2023-07-05 00:20:37 +0700

    upgrade gpt

--- docker-run.sh
+++ docker-run.sh
@@ -1,5 +1,6 @@
 # docker build -t api_ai_chatbot:tag -f Dockerfile . #1337
-docker image build -t api_ai_chatbot:tag . #5000
+GPT="gpt4"
+docker image build -t ${GPT}:tag . #5000
 
 # docker run -d -p 5000 api_ai_chatbot:tag
 docker stop $(docker ps -a -q)
@@ -9,7 +10,7 @@ COUNT=3
 PORT=1337
 for i in $(seq 1 $COUNT)
 do
-    docker run -d -p ${PORT}:1337 api_ai_chatbot:tag
+    docker run -d -p ${PORT}:1337 ${GPT}:tag
     PORT=$((PORT+1))
 done
 # docker ps -al
