55714a227339267a1150d71975ca05c4b450fdf5
iphearum <phearumgic@gmail.com>
2023-10-23 23:01:49 +0700

    update post image

--- docker-run.sh
+++ docker-run.sh
@@ -1,13 +1,15 @@
 GPT="gpt4"
 # docker run -d -p 5000 api_ai_chatbot:tag
+
 docker stop $(docker ps -a -q) #stop container
 docker rm -f $(docker ps -a -q) # remove container
 
-# docker rmi -f $(docker ps -a -q) # remove image
-# docker system prune -a # remove all container and image
+docker system prune -a # remove all container and image
 
-# docker image build -t ${GPT}img:tag -f Dockerfile1 . #1337
-# docker image build -t ${GPT}:tag . #5000
+# docker rmi -f $(docker ps -a -q) # remove image
+
+docker image build -t ${GPT}img:tag -f Dockerfile1 . #1337
+docker image build -t ${GPT}:tag . #5000
 
 
 COUNT=6
