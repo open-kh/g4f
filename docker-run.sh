dffbd8efbf44c92d29f87e48f4620b50a3d76c22
iphearum <phearumgic@gmail.com>
2023-10-24 08:47:25 +0700

    upgrade api 2023-10-24 08:47:25

--- docker-run.sh
+++ docker-run.sh
@@ -5,8 +5,10 @@ docker stop $(docker ps -a -q) #stop container
 docker rm -f $(docker ps -a -q) # remove container
 
 GPT="gpt4"
-docker image build -t ${GPT}img:tag -f Dockerfile1 . #1337
-docker image build -t ${GPT}:tag . #5000
+docker image build -t gpt4img:tag -f Dockerfile1 . #1337
+docker image build -t gpt4:tag . #5000
+# docker image build -t ${GPT}img:tag -f Dockerfile1 . #1337
+# docker image build -t ${GPT}:tag . #5000
 
 # docker system prune -a # remove all container and image
 
@@ -14,10 +16,11 @@ COUNT=6
 PORT=1337
 for i in $(seq 1 $COUNT)
 do
-    docker run -d -p ${PORT}:1337 ${GPT}:tag
+    docker run -d -p ${PORT}:1337 gpt4:tag
     PORT=$((PORT+1))
 done
-docker run -d -p 1333:1333 ${GPT}img:tag
+
+docker run -d -p 1333:1333 gpt4img:tag
 # docker run -d -p 1342:1333 ${GPT}:tag
 
 # docker ps -al
