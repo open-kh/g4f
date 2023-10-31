7d26e9e4b309e906166b1d4fc759b413d57a4d23
iphearum <phearumgic@gmail.com>
2023-11-01 03:55:17 +0700

    upgrade api 2023-11-01 03:55:17

---
+++ etc/testing/runner.py
@@ -0,0 +1,22 @@
+import json
+from g4f.Provider import (Perplexity)
+
+perplexity = Perplexity()
+question = "Hello, What is your name"
+answer = perplexity.search(question)
+count = 0
+for doc in answer:
+    print(doc)
+    # doc_len = len(doc["chunks"])
+    # content = "".join(doc["chunks"][-(doc_len-count):] if doc_len > count else doc["chunks"])
+    # count = doc_len
+    # if doc["status"] is "completed":
+        # data = {
+        #     "status": doc.status,
+        #     "uuid": doc.uuid,
+        #     "media_items": doc.media_items,
+        #     "query_str": doc.query_str,
+        # }
+        # print(json.dumps(a, separators=(',', ':')))
+
+perplexity.close()
