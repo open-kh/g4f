7610f554bef00ec41a004619fc3ac336f452b255
iphearum <phearumgic@gmail.com>
2023-11-04 16:00:59 +0700

    upgrade api 2023-11-04 16:00:59

--- docker-run.sh
+++ docker-run.sh
@@ -1,12 +1,13 @@
 # docker run -d -p 5000 api_ai_chatbot:tag
 # docker rmi -f $(docker ps -a -q) # remove image
+docker volume create gpt4_data
 
 docker stop $(docker ps -a -q) #stop container
 docker rm -f $(docker ps -a -q) # remove container
 
-GPT="gpt4"
+# GPT="gpt4"
 docker image build -t gpt4:tag . #5000
-docker image build -t gpt4img:tag -f Dockerfile1 . #1337
+# docker image build -t gpt4img:tag -f Dockerfile1 . #1337
 # docker image build -t ${GPT}img:tag -f Dockerfile1 . #1337
 # docker image build -t ${GPT}:tag . #5000
 
@@ -16,11 +17,11 @@ COUNT=6
 PORT=1337
 for i in $(seq 1 $COUNT)
 do
-    docker run -d -p ${PORT}:1337 gpt4:tag
+    docker run -d -p ${PORT}:1337 -v gpt4_data:/out gpt4:tag
     PORT=$((PORT+1))
 done
 
-docker run -d -p 1333:1333 gpt4img:tag
+# docker run -d -p 1333:1333 gpt4img:tag
 # docker run -d -p 1342:1333 ${GPT}:tag
 
 sh docker-reload.sh
--- interference/app.py
+++ interference/app.py
@@ -1,12 +1,14 @@
+import os
 import json
 import time
 import random
 import string
 from tokenize import String
+from urllib.request import urlretrieve
 
-from flask        import Flask, request, Response
+from flask        import Flask, request, Response, send_file
 from flask_cors   import CORS
-from g4f          import ChatCompletion, models, Provider
+from g4f          import ChatCompletion, models
 from g4f.Provider import (
     Bard,
     PerplexityAI,
@@ -14,12 +16,14 @@ from g4f.Provider import (
     Bing,
     HuggingChat,
     Llama2,
+    StabilityAI,
 )
-from g4f.Provider.helper import get_cookies
 
 app = Flask(__name__)
 CORS(app)
 
+REPLICATE_API_TOKEN="r8_clGIfdHF8p7zDWF3LBz1Ri8WQT7xNBU2BqAb2"
+os.environ["REPLICATE_API_TOKEN"]=REPLICATE_API_TOKEN
 public_key = "pJNAtlAqCHbUDTrDudubjSKeUVgbOMvkRQWMLtscqsdiKmhI"
 
 class ResModel:
@@ -144,6 +148,41 @@ def chat_completions():
         yield f'data: {content}\n\n'
 
     return app.response_class(streaming(), mimetype='text/event-stream')
+
+@app.route("/chat/image_generation", methods=['POST'])
+def image_generate_temp():
+    ai = StabilityAI()
+    text = request.json.get('text')
+    barer = request.headers.get('Authorization')
+    if barer is None:
+        barer = 'unknown'
+    else:
+        barer = barer.strip().split(" ")[1] if len(barer.strip().split(" ")) > 1 else 'unknown'
+
+    if barer != f"pk-{public_key}":
+        return Response(response='Unauthorized', status=401)
+
+    out = ai.image_generate(prompt=f"{text}, cinematic, dramatic")
+    images = []
+    for img in out['images']:
+        imgID = ''.join(random.choices('ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789', k=28))
+        imgName = f"txt2img_{imgID}.png"
+        urlImg = f"https://{request.host}/images/{imgName}"
+        urlretrieve(img, f"./out/{imgName}")
+        # size = "{height=270px width=270px}" 
+        # if len(out['images'])>=2 else ""
+        images.append(f"[![{out['prompt']}]({urlImg})]({urlImg})")
+
+    token = "\n".join(images)
+
+    completion_data = f"Sure, Here is the image:\n{token}\nDid you like it?"
+
+    return Response(completion_data, content_type='application/json')
+
+
+@app.route('/images/<path:path>',methods=['GET'])
+def send_static_file(path):
+    return send_file(f"/out/{path}")
 
 def main():
     app.run(host='0.0.0.0', port=1337, debug=True)
