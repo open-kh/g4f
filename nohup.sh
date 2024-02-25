b0831b3737e073461e3a6bfa4fd47905d7b51f41
iphearum <phearumgic@gmail.com>
2024-02-25 21:57:39 +0700

    unchange

--- nohup.sh
+++ nohup.sh
@@ -3,7 +3,7 @@ COUNT=6
 PORT=1337
 for i in $(seq 1 $COUNT)
 do
-    nohub python -m interference.app ${PORT} >/dev/null 2>&1 &
+    nohup python -m interference.app ${PORT} >/dev/null 2>&1 &
     echo "app ${PORT}"
     PORT=$((PORT+1))
 done
