cc6a25c431354a3bb5931e017b509e0b8aeed4ab
iphearum <phearumgic@gmail.com>
2024-02-25 22:18:54 +0700

    upgrade api 2024-02-25 22:18:54

--- interference/app.py
+++ interference/app.py
@@ -149,7 +149,7 @@ def send_static_file(path):
     return send_file(f"/out/{path}")
 
 def main(port:int=1337):
-    app.run(host='127.0.0.1', port=port, debug=True)
+    app.run(host='0.0.0.0', port=port, debug=True)
 
 if __name__ == '__main__':
     arguments = sys.argv[1:]
